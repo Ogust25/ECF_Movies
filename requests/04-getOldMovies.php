2beaf786b479eb93370537b04222c9dfb21fc786
Ogu <august.gros@gmail.com>
2022-06-07 11:57:50 +0200

    fix request old movies

--- requests/04-getOldMovies.php
+++ requests/04-getOldMovies.php
@@ -5,7 +5,8 @@ require_once('../connect.php');
 // Réaliser une requête qui permet de récupérer tous les films d'avant 2002
 
 $oldMovie = $db->prepare('SELECT infos_movies.title, release_year FROM infos_movies
-                        INNER JOIN `release_years` ON infos_movies.id = release_years.id
+                        INNER JOIN `release_years` ON infos_movies.release_years_id = release_years.id
                         WHERE release_year < 2002');
 $oldMovie->execute();
 $res_oldMovie = $oldMovie->fetchAll(PDO::FETCH_ASSOC);
+var_dump($res_oldMovie);
