9b95cc2c9af36bd22be9b627aea7bd173047eeb7
Ogu <august.gros@gmail.com>
2022-06-07 10:30:00 +0200

    add request title

--- requests/01-getTitleMovies.php
+++ requests/01-getTitleMovies.php
@@ -1,3 +1,9 @@
 <?php
 
 // Réaliser une requête qui permet de récupérer tous les titres des films
+
+require_once('../connect.php');
+
+$title = $db->prepare('SELECT title FROM `infos_movies`');
+$title->execute();
+$res = $title->fetch(PDO::FETCH_ASSOC);
