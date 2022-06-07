04e49b7c96aa27cd6a18aaea48b2fb08051eb2a8
Ogu <august.gros@gmail.com>
2022-06-07 11:14:01 +0200

    add request diff film date

--- requests/03-getDifference.php
+++ requests/03-getDifference.php
@@ -1,3 +1,9 @@
 <?php
 
+require_once('../connect.php');
+
 // Réaliser une requête affichant le nombre d'années entre le film le plus récent et le film le plus vieux
+
+$diffFilmDate = $db->prepare('SELECT MAX(release_year) - MIN(release_year) FROM `release_years`');
+$diffFilmDate->execute();
+$res_diffFilmDate = $diffFilmDate->fetchAll(PDO::FETCH_ASSOC);
