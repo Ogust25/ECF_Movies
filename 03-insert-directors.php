31afee51f62bc08f9991456b3633edb899ef0b8f
Ogu <august.gros@gmail.com>
2022-06-07 09:08:54 +0200

    fix script directors

--- 03-insert-directors.php
+++ 03-insert-directors.php
@@ -11,8 +11,9 @@ if ($file !==FALSE) {
     while(($row = fgetcsv($file, null, ";", "'", "\n"))!== FALSE){
         $one_dir = explode (",", $row[3]);
         for ($i=0; $i < count($one_dir) ; $i++) { 
-            $req_dir->bindValue(':name_director', $one_dir[$i], PDO::PARAM_INT);
+            $req_dir->bindValue(':name_director', $one_dir[$i], PDO::PARAM_STR);
             $req_dir->execute();
+            //var_dump($one_dir[$i]);
         }
     }
 }
